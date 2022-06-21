6bc398bdc82ef96a7aaf82607db8c2764044033d
riku79264 <riku.tanimoto.tani@gmail.com>
2022-06-21 16:01:48 +0900

    imageをFactoryBotで生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name {Faker::Lorem.sentence}
     explain {Faker::Lorem.sentence}
-    # image {Faker::Lorem.sentence}
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'public/image/output-image1.png'))}
     category_id           {2}
     status_id             {2}
     postage_id            {2}
@@ -11,5 +11,4 @@ FactoryBot.define do
     price                 {3000}      
     association :user    
   end
-  
 end
