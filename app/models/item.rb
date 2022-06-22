6229f119486310a37529de659ace361c7ceb1512
riku79264 <riku.tanimoto.tani@gmail.com>
2022-06-22 15:47:14 +0900

    プルリクエスト修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
 
 
   belongs_to :user
-  has_one :purchase_record
+  # has_one :purchase_record
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
